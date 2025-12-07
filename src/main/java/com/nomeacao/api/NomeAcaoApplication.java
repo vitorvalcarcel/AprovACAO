277c21dcb11247f504a3c828fe84ee1cb737e55c
Vitor Valcarcel <vitorvalcarcel@gmail.com>
2025-12-07 10:55:06 -0300

    feat: estrutura inicial do projeto

---
+++ src/main/java/com/nomeacao/api/NomeAcaoApplication.java
@@ -0,0 +1,13 @@
+package com.nomeacao.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NomeAcaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NomeAcaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nomeacao/api/NomeAcaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nomeacao.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NomeAcaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
