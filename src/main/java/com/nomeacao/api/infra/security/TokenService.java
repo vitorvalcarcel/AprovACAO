102b5e7babe568672501b4058f2cf5e9ab9a726b
Vitor Valcarcel <vitorvalcarcel@gmail.com>
2025-12-08 12:54:43 -0300

    Ajuste falha de comp

--- src/main/java/com/nomeacao/api/infra/security/TokenService.java
+++ src/main/java/com/nomeacao/api/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package com.nomeacao.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.nomeacao.api.model.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
